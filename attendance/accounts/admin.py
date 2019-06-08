a36546b3a10e9c4b2163375b9496caac3079ec9a
miltonbhowmick <miltonbhowmick6@gmail.com>
2019-06-09 01:56:21 +0600

    search fields for admin

--- attendance/accounts/admin.py
+++ attendance/accounts/admin.py
@@ -9,9 +9,19 @@ admin.site.register(UserProfile)
 class ClassSessionAdmin(admin.ModelAdmin):
 	prepopulated_fields = {'slug': ('session',)}
 
+class CourseCodeAdmin(admin.ModelAdmin):
+	search_fields = ('course_code',)
+
+class StudeneProfileAdmin(admin.ModelAdmin):
+	search_fields = ('student_id','student_user__first_name','student_user__last_name','student_user__username','student_user__email')
+class PresentSheetAdmin(admin.ModelAdmin):
+	search_fields = ('join_date',)
+class CoursePercentageAdmin(admin.ModelAdmin):
+	search_fields = ('student_user__first_name','student_user__last_name','student_user__username','student_user__email')
+
 admin.site.register(ClassSession)
-admin.site.register(CourseCode)
-admin.site.register(StudentProfile)
+admin.site.register(CourseCode, CourseCodeAdmin)
+admin.site.register(StudentProfile, StudeneProfileAdmin)
 admin.site.register(TeacherProfile)
-admin.site.register(PresentSheet)
-admin.site.register(CoursePercentage)
+admin.site.register(PresentSheet, PresentSheetAdmin)
+admin.site.register(CoursePercentage, CoursePercentageAdmin)
